2e77cd0e23ad754344b52de2925ea195cdd7487c
sandhyapilli <pillisandhya410@gmail.com>
2025-02-08 12:23:02 +0530

    Add files via upload

---
+++ OnlineLoanManagement/src/main/java/com/example/LoggingDemo/OnlineLoanManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoggingDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLoanManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLoanManagementApplication.class, args);
+	}
+
+}
