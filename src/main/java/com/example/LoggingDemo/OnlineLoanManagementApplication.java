c8c69f5ad7567af39fa3fcdf5b58764d5fd3e475
sandhyapilli <pillisandhya410@gmail.com>
2025-02-10 17:14:59 +0530

    just tried

--- src/main/java/com/example/LoggingDemo/OnlineLoanManagementApplication.java
+++ src/main/java/com/example/LoggingDemo/OnlineLoanManagementApplication.java
@@ -8,6 +8,6 @@ public class OnlineLoanManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineLoanManagementApplication.class, args);
-	}
+	}//started
 
 }
